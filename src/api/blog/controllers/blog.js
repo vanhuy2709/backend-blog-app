f5557dd08cf9f1618dffec9800584e08683559c8
hvhuy279 <hvhuy279@gmail.com>
2024-08-26 18:15:55 +0700

    feat/build-backend-blog-app

---
+++ src/api/blog/controllers/blog.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * blog controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.db.query('api::blog.blog').findOne({
+      where: { id },
+      populate: ['image']
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
